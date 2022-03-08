6f8d872b341ca0908ba1d93e86e8eed33e54f8f0
Rv <panditravi217@gmail.com>
2022-03-08 19:24:11 +0545

    static dj adds

--- deepfake_detection/wsgi.py
+++ deepfake_detection/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deepfake_detection.settings')
+from dj_static import Cling, MediaCling
 
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
