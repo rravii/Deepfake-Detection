5839ee0d4898465eb25af29456af278d292f69ae
Rv <panditravi217@gmail.com>
2022-03-08 20:24:21 +0545

    cors added

--- deepfake_detection/settings.py
+++ deepfake_detection/settings.py
@@ -58,12 +58,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'deepfake_detection.urls'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOWED_ORIGINS = [
+    '*',
+]
 
 TEMPLATES = [
     {
